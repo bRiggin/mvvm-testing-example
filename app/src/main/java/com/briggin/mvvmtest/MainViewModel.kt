75ec1042e6babbff851a466b4af2a58443cb746f
Rhys <rhysbiggin@gmail.com>
2020-10-31 22:17:43 +0000

    Implement testable but very manual MVVM implementation

--- app/src/main/java/com/briggin/mvvmtest/MainViewModel.kt
+++ app/src/main/java/com/briggin/mvvmtest/MainViewModel.kt
@@ -2,33 +2,48 @@ package com.briggin.mvvmtest
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    private val _state: MutableLiveData<State> = MutableLiveData()
-    val title: LiveData<Int> = Transformations.distinctUntilChanged(Transformations.map(_state) { it.title })
-    val subTitle: LiveData<Int> = Transformations.distinctUntilChanged(Transformations.map(_state) { it.subTitle })
-    val description: LiveData<Int> = Transformations.distinctUntilChanged(Transformations.map(_state) { it.description })
-    val progressBar: LiveData<Int> = Transformations.distinctUntilChanged(Transformations.map(_state) { it.progressBar })
-    val checkbox: LiveData<Boolean> = Transformations.distinctUntilChanged(Transformations.map(_state) { it.checkBox })
-    val switch: LiveData<Boolean> = Transformations.distinctUntilChanged(Transformations.map(_state) { it.switch })
-    val showProgressBar: LiveData<Int> = Transformations.distinctUntilChanged(Transformations.map(_state) { it.showProgress })
+    private val _title: MutableLiveData<Int> = MutableLiveData()
+    val title: LiveData<Int> = _title
+    private val _subTitle: MutableLiveData<Int> = MutableLiveData()
+    val subTitle: LiveData<Int> = _subTitle
+    private val _description: MutableLiveData<Int> = MutableLiveData()
+    val description: LiveData<Int> = _description
+    private val _progressBar: MutableLiveData<Int> = MutableLiveData()
+    val progressBar: LiveData<Int> = _progressBar
+    private val _checkbox: MutableLiveData<Boolean> = MutableLiveData()
+    val checkbox: LiveData<Boolean> = _checkbox
+    private val _switch: MutableLiveData<Boolean> = MutableLiveData()
+    val switch: LiveData<Boolean> = _switch
+    private val _showProgressBar: MutableLiveData<Int> = MutableLiveData()
+    val showProgressBar: LiveData<Int> = _showProgressBar
 
     fun setStateOne() {
-        _state.postValue(StateOne)
+        updateView(StateOne)
     }
 
     fun setStateTwo() {
-        _state.postValue(StateTwo)
+        updateView(StateTwo)
     }
 
     fun setStateThree() {
-        _state.postValue(StateThree)
+        updateView(StateThree)
     }
 
     fun setStateFour() {
-        _state.postValue(StateFour)
+        updateView(StateFour)
+    }
+
+    private fun updateView(state: State) {
+        _title.postValue(state.title)
+        _subTitle.postValue(state.subTitle)
+        _description.postValue(state.description)
+        _progressBar.postValue(state.progressBar)
+        _checkbox.postValue(state.checkBox)
+        _switch.postValue(state.switch)
+        _showProgressBar.postValue(state.showProgress)
     }
 }
